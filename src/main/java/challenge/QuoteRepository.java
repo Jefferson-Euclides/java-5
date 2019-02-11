6d3bbc9128c6dbf5af54cd95544a44d6decd92c3
Jefferson-random <jeffersoneuclides83@gmail.com>
2019-02-11 17:49:03 -0200

    primeiro commit

---
+++ src/main/java/challenge/QuoteRepository.java
@@ -0,0 +1,12 @@
+package challenge;
+
+import org.h2.tools.Script;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuoteRepository extends JpaRepository<Quote, Integer> {
+
+    List<Quote> findByActor(String actor);
+
+}
---
+++ src/main/java/challenge/QuoteServiceImpl.java
@@ -0,0 +1,34 @@
+package challenge;
+
+import org.h2.tools.Script;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class QuoteServiceImpl implements QuoteService {
+
+	@Autowired
+	private QuoteRepository repository;
+
+	@Override
+	public Quote getQuote() {
+		List<Quote> listQuotes = repository.findAll();
+		Random random = new Random();
+		Quote quote = listQuotes.get(random.nextInt(listQuotes.size()-1));
+		while(quote.getActor() == null) {
+			quote = listQuotes.get(random.nextInt(listQuotes.size()-1));
+		}
+		return quote;
+	}
+
+	@Override
+	public Quote getQuoteByActor(String actor) {
+		List<Quote> listQuotes = repository.findByActor(actor);
+		Random random = new Random();
+		return listQuotes.get(random.nextInt(listQuotes.size()-1));
+	}
+
+}
